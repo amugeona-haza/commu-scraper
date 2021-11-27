1a2e7798e250c3f75174c52133c3cc53ce42c260
eggplantiny <eggplantiny@gmail.com>
2021-11-28 01:28:20 +0900

    update runner

--- src/news/runner.ts
+++ src/news/runner.ts
@@ -4,17 +4,22 @@ import { getExponentiallyUniform } from '../helpers/random'
 import { delay } from '../helpers/asyncUtils'
 import moment = require('moment')
 import * as fs from 'fs'
+import { Moment } from 'moment'
 
 const MAXIMUM_PAGE = 50
 const QUERY = '이재명'
-const START_DATE = moment()
 
 function writeJson (name: string, data: any) {
   fs.writeFileSync(name, JSON.stringify(data, null, 2))
 }
 
+function generateDateRange (date: Moment) {
+  const from = moment(date).subtract(1, 'week').format('YYYYMMDD')
+  const to = moment(date).format('YYYYMMDD')
+  return { from, to }
+}
+
 async function runner (from: string, to: string, pageNo: number = 1, successList: Article[] = [], failedList: string[] = []) {
-  const triedArticleSize = successList.length + failedList.length
   console.log(`> [${from} ~ ${to}] scraping ${pageNo} page with query '${QUERY}'`)
   const { success, failed } = await scrapPage(QUERY, pageNo, from, to)
 
@@ -34,14 +39,26 @@ async function runner (from: string, to: string, pageNo: number = 1, successList
 }
 
 async function run () {
+  const START_DATE = moment()
+  const date = moment()
+
+  const success = []
+  const failed = []
   console.log(`start scraping with query ${QUERY}.`)
   console.time('time')
-  const { successList, failedList } = await runner('20211121', '20211128')
+  do {
+    const { from, to } = generateDateRange(date)
+    const { successList, failedList } = await runner(from, to)
+    success.push(...successList)
+    failed.push(...failedList)
+    date.subtract(1, 'week')
+    console.log(`success: ${success.length}, failed: ${failed.length}`)
+  } while (START_DATE.diff(date, 'month') <= 6)
   console.log(`complete scraping with query ${QUERY}`)
   console.timeEnd('time')
 
-  writeJson(`${QUERY}_success.json`, successList)
-  writeJson(`${QUERY}_failed.json`, failedList)
+  writeJson(`${QUERY}_success.json`, success)
+  writeJson(`${QUERY}_failed.json`, failed)
 }
 
 run()
