63603f7a1f35ebd324edab55c2624503ceed9a11
eggplantiny <eggplantiny@gmail.com>
2021-11-29 19:07:20 +0900

    change config

--- src/news/runner.ts
+++ src/news/runner.ts
@@ -7,9 +7,10 @@ import { Moment } from 'moment'
 import * as fs from 'fs'
 
 //  query constants
-const QUERY = '이재명'
+const QUERY = '윤석열'
 const END_CONDITION_AMOUNT = 6
 const END_CONDITION_UNIT_OF_TIME = 'month'
+const START_DATE_STRING = moment().format('YYYYMMDD')
 
 //  settings
 const DATE_RANGE_AMOUNT = 1
@@ -47,26 +48,26 @@ async function runner (from: string, to: string, pageNo: number = 1, successList
 }
 
 async function run () {
-  const START_DATE = moment()
-  const date = moment()
-
+  const startDate = moment(START_DATE_STRING)
+  const cursorDate = moment()
   const success = []
   const failed = []
+
   console.log(`start scraping with query ${QUERY}.`)
   console.time('time')
   do {
-    const { from, to } = generateDateRange(date)
+    const { from, to } = generateDateRange(cursorDate)
     const { successList, failedList } = await runner(from, to)
     success.push(...successList)
     failed.push(...failedList)
-    date.subtract(DATE_RANGE_AMOUNT, DATE_RANGE_UNIT)
+    cursorDate.subtract(DATE_RANGE_AMOUNT, DATE_RANGE_UNIT)
     console.log(`success: ${success.length}, failed: ${failed.length}`)
-  } while (START_DATE.diff(date, END_CONDITION_UNIT_OF_TIME) <= END_CONDITION_AMOUNT)
+  } while (startDate.diff(cursorDate, END_CONDITION_UNIT_OF_TIME) < END_CONDITION_AMOUNT)
   console.log(`complete scraping with query ${QUERY}`)
   console.timeEnd('time')
 
-  writeJson(`${QUERY}_success.json`, success)
-  writeJson(`${QUERY}_failed.json`, failed)
+  writeJson(`${QUERY}_success.data.json`, success)
+  writeJson(`${QUERY}_failed.data.json`, failed)
 }
 
 run()
