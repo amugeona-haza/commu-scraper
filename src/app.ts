55f49036f28f406ff16cc71b89310166f32f71e5
eggplantiny <eggplantiny@gmail.com>
2023-02-03 21:44:32 +0900

    good

--- src/app.ts
+++ src/app.ts
@@ -15,17 +15,17 @@ interface IResult {
 
   const resultList: IResult[] = []
   for await (const scraper of scrapers) {
-    const data = await scraper.fetch('1', '54298209')
+    const data = await scraper.fetch('1', '60264614')
 
     const result: IResult = {
       name: scraper.name,
       site: scraper.site,
       data
     }
-    
+
     resultList.push(result)
   }
-  
+
   console.log(`${resultList.length} items scrapped`)
 
   fs.writeFileSync('data.json', JSON.stringify(resultList, null, 2))
--- src/scrapers/modules/ruliwebHumorBest.ts
+++ src/scrapers/modules/ruliwebHumorBest.ts
@@ -13,27 +13,27 @@ export default class RuliwebHumorBestScraper extends Scraper {
   constructor () {
     super('Ruliweb Humor Best', 'ruliweb')
   }
-  
+
   async fetchArticleInfoList (page: string | number) {
     const { data: html } = await axios.get<string>(`https://bbs.ruliweb.com/best/humor/now?page=${page}`)
     const $ = cheerio.load(html)
-  
+
     const result: IArticleInfo[] = []
-  
-    $('#best_body > table > tbody').find('tr > td.subject > a').each(function (index, elem) {
-      const title = $(this).text()
-      const url = $(this).attr('href')
+
+    $('#best_body > table > tbody > tr').each(function (index, elem) {
+      const title = $(this).find('a').text().split('\n')[2].trim().replace(/\(.+\)/g, '').trim()
+      const url = $(this).find('a').attr('href')
       const id = url.split('/').pop()
       result.push({ title, url, id })
     })
-  
+
     return result
   }
-  
+
   async fetchArticle (url: string, articleId: string) {
     const { data: html } = await axios.get<string>(url)
     const $ = cheerio.load(html)
-  
+
     //  공통정보
     const title = $('#board_read > div > div.board_main > div.board_main_top > div.user_view > div > h4 > span').text().trim()
     const author = $('#board_read > div > div.board_main > div.board_main_top > div.user_view > div.row.user_view_target > div.col.user_info_wrapper > div > p:nth-child(1) > strong').text().trim()
@@ -41,7 +41,7 @@ export default class RuliwebHumorBestScraper extends Scraper {
     const date = $('#board_read > div > div.board_main > div.board_main_top > div.user_view > div.row.user_view_target > div.col.user_info_wrapper > div > p:nth-child(6) > span').text().trim()
     const upVote = Number($('#board_read > div > div.board_main > div.board_main_view > div.row > div > div > div.like > span').text().trim()) || 0
     const downVote = Number($('#board_read > div > div.board_main > div.board_main_view > div.row > div > div > div.dislike > span').text().trim()) || 0
-  
+
     // Paragraphs
     const paragraphs: IParagraph[] = []
     $('#board_read > div > div.board_main > div.board_main_view > div.view_content > article > div > p')
@@ -50,7 +50,7 @@ export default class RuliwebHumorBestScraper extends Scraper {
           type: 'text',
           value: ''
         }
-      
+
         if ($(this).has('img').length > 0) {
           paragraph.type = 'image'
           paragraph.value = $(this).find('img').attr('src')
@@ -80,7 +80,7 @@ export default class RuliwebHumorBestScraper extends Scraper {
         }
         paragraphs.push(paragraph)
       })
-  
+
     //  Comments
     const comments: IComment[] = []
     $('#cmt > div.comment_view_wrapper.row > div.comment_view.normal > table > tbody > tr')
@@ -88,12 +88,12 @@ export default class RuliwebHumorBestScraper extends Scraper {
         const text = $(this).find('td.comment > div.text_wrapper > span.text').text().trim()
         const author = $(this).find('td.user > div > div').text().trim()
         const userId = $(this).find('td.user > div > span > a').text().trim()
-      
+
         const result: IComment = { text, author, userId }
         comments.push(result)
       })
-  
-  
+
+
     const result: IArticle = {
       title,
       url,
@@ -106,10 +106,10 @@ export default class RuliwebHumorBestScraper extends Scraper {
       paragraphs,
       comments
     }
-  
+
     return result
   }
-  
+
   generateNextPage (page: string) {
     return `${Number(page) + 1}`
   }
